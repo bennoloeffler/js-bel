69107ef4cab498578c3acded4a4b0c97ac15ceec
bennoloeffler <benno.loeffler@gmx.de>
2024-03-10 09:19:12 +0100

    favicon and js toggle

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ console.log("hello js");
 
 function sqrt(i) {return i*i}
 
+let toggle = true
+
 function change(i) {
-    document.getElementById("theOne").innerHTML= `sqrt (${i}) = ${sqrt(i)}`
+	if (toggle) {
+    	document.getElementById("theOne").innerHTML = `sqrt (${i}) = ${sqrt(i)}`
+	} else {
+		document.getElementById("theOne").innerHTML = `some other text`
+	}
+	console.log("toggle:" + toggle);
+	toggle = !toggle
 }
